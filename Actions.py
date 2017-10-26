bd142f1a0eab89cc6e10188d3b9096d9620c2397
Tom Evans <TomosAMEvans@gmail.com>
2017-10-26 23:12:03 +0100

    Fixed #2: Commented the Movement and Action scrpts

--- Actions.py
+++ Actions.py
@@ -4,14 +4,32 @@ from typing import Tuple
 
 
 class Action(ABC):
+    """
+    An abstract class that all things that can be composed together must
+    instanciate.
+
+        eg. Move, Interact, etc.
+    """
     pass
 
 class Composite(Action):
+    """
+    An Action that is made by composing one or more Actions together in a
+    sequential manner.
+    """
     actions:Tuple[Action]
 
     def __init__(self, *actions):
+        """
+        Initalizes a composite action.
+
+        Params:
+            A variable number of actions.
+        """
         self.actions = actions
 
     def __str__(self):
-
+        """
+        The string representation of an Action Composite.
+        """
         return "("+ ". \n".join(map(str, self.actions)) +")"
--- Movement.py
+++ Movement.py
@@ -1,67 +1,102 @@
-"""
-TODO: Document
-"""
-
 from enum import Enum
 from Actions import Action
 from abc import ABC
 
 class Speed(Enum):
+    """
+    Represents the differnt speeds that the user can specify.
+    """
     SLOW = 0
     MED  = 1
     FAST = 2
 
     def __str__(self):
+        """
+        Defines the string representation of a Speed.
+        """
         output = ["slowly", "", "quickly"]
         return output[self.value]
 
 
 
 class Stance(Enum):
+    """
+    Represents the different stances that the user can specify.
+    """
     PRONE  = 0
     CROUCH = 1
     STAND  = 2
 
     def __str__(self):
+        """
+        Defines the string representation of a Stance.
+        """
         output = ["crawl", "crouch", "walk"]
         return output[self.value]
 
 
 class Direction(Enum):
+    """
+    Represents the different the directions that the user can use.
+    """
     LEFT      = 0
     RIGHT     = 1
     BACKWARDS = 2
     FORWARDS  = 3
 
     def __str__(self):
+        """
+        Defines the string representation of a Direction.
+        """
         output = ["on the left", "on the right", "behind you", "straight ahead"]
         return output[self.value]
 
 
 class Location(ABC):
+    """
+    The abstract Location class.
+    """
     pass
 
 
 class Absolute(Location):
+    """
+    Represents an absolute location.
+        eg. In the Building
+    """
     pass
 
     def __str__(self):
+        """
+        Defines the string representation of an Absolute location.
+        """
         return " in the building"
 
 
 class Contextual(Location):
+    """
+    Represents a contextual location.
+        eg. The 3rd door on your right.
+    """
     direction: Direction # e.g. on the right
-    num: int             # e.g. 3rd (door)
+    num      : int       # e.g. 3rd (door)
 
     def __init__(self, direction: Direction = Direction.FORWARDS, num:int = 0):
         self.direction = direction
         self.num = num
 
     def __str__(self):
+        """
+        Defines the string representation Contextual location.
+        """
         return str(self.num+1)+ "th " + str(self.direction)
 
 
 class Object():
+    """
+    Represents a generic Object.
+        eg. Table.
+    """
     name    : str
     location: Location
 
@@ -70,10 +105,17 @@ class Object():
         self.location = location
 
     def __str__(self):
+        """
+        Defines the string representation of an Object.
+        """
         return self.name + " which is " + str(self.location)
 
 
 class Relative(Object):
+    """
+    Represents a relative object.
+        eg. Half way along the corridor.
+    """
     to        : Object
     proportion: float
 
@@ -82,6 +124,9 @@ class Relative(Object):
         self.proportion = proportion
 
     def __str__(self):
+        """
+        Defines the string representation of a Relative location.
+        """
         if self.proportion > 0.8:
             amount = "the end of"
         elif 0.2 <= self.proportion <= 0.8:
@@ -92,15 +137,27 @@ class Relative(Object):
         return amount + " " + str(self.to)
 
 class StartPos(Enum):
+    """
+    Represents the direction of memory access in Rememebered objects.
+        eg. The first door you went in  -> START
+        eg. The Last door you went in   -> END
+    """
     START = 0
     END   = 1
 
     def __str__(self):
+        """
+        Defines the string representation of a StartPos.
+        """
         output = ["START", "END"]
         return output[self.value]
 
 
 class MemoryIndex():
+    """
+    Represents an offset and a starting end of an array used to
+    stor the systems memory.
+    """
     offset: int
     start: StartPos
 
@@ -109,22 +166,35 @@ class MemoryIndex():
         self.start = start
 
     def __str__(self):
+        """
+        Defines the string representation of a MemoryIndex.
+        """
         return "("+ str(self.start)+ ", "+ str(self.offset) +")"
 
 
 class Rememebered(Object):
-    obj: Object
-    index: MemoryIndex
+    """
+    Represents an Object that has been visited before, and hense can be moved
+    'back' to, for example.
+    """
+    obj   : Object
+    index : MemoryIndex
 
     def __init__(self, obj:Object, index:MemoryIndex):
         self.obj = obj
         self.index = index
 
     def __str__(self):
+        """
+        Defines the string representation of a Rememebered Object.
+        """
         return "(" + str(self.obj) + ", " + str(self.index) + ")"
 
 
 class Move(Action):
+    """
+    Represents a valid move.
+    """
     speed : Speed
     stance: Stance
     dest  : Object
@@ -135,4 +205,7 @@ class Move(Action):
         self.dest   = to
 
     def __str__ (self):
+        """
+        Defines the string representation of a Move.
+        """
         return str(self.speed) + " " + str(self.stance) + " to " + str(self.dest)
