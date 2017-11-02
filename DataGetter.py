61ef448965c494904ea151c2eadc6774cdfc2751
Tom Evans <TomosAMEvans@gmail.com>
2017-11-02 12:50:18 +0000

    Made the data getter into a class for easy of use

--- DataGetter.py
+++ DataGetter.py
@@ -1,16 +1,11 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+class DataGetter():
+    def __init__(self, credsFile='client_secret.json'):
+        self.scope  = ['https://spreadsheets.google.com/feeds']
+        self.creds  = ServiceAccountCredentials.from_json_keyfile_name(credsFile, self.scope)
+        self.client = gspread.authorize(self.creds)
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
-
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("MovementTrainingData").sheet1
-
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+    def basicMovementData(self):
+        return self.client.open("MovementTrainingData").sheet1.get_all_records()
